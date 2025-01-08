be56c31192b85799c4743c470dcd70f0834ba3ae
sinaanzin <128033761+sinaanzin@users.noreply.github.com>
2025-01-08 14:38:09 +0530

    Add files via upload

---
+++ as1.py
@@ -0,0 +1,57 @@
+print("Bob")
+print("ST1001")
+print("bob@gmail.com")
+
+print("Bob\nST1001\nbob@gmail.com")
+
+num1=5
+num2=10
+print(f"{num1} + {num2} = {num1+num2}")
+print(f"{num1} - {num2} = {num1-num2}")
+print(f"{num1} * {num2} = {num1*num2}")
+print(f"{num1} / {num2} = {num1/num2}")
+
+for i in range (1,6):
+    print(i)
+
+print("\"SDK\" stands for \"Software Development Kit\", whereas \"IDE\" stands for"
+      " \"Integrated Development Environment\".")
+
+print("python is an \"awesome\" language.")
+print("python\n\t2025")
+print('I\'m from Entri.\b')
+print("\65")
+print("\x65")
+print("Entri", "2023", sep="\n")
+print("Entri", "2023", sep="\b")
+print("Entri", "2023", sep="*", end="\b\b\b\b")
+
+print("\n")
+
+num=23
+text_num ="57"
+decimal =98.3
+print("type of num",type(num))
+print("type of textnum",type(text_num))
+print("type of decimal",type(decimal))
+
+
+days_in_year = 365
+hours_in_day = 24
+minutes_in_hour = 60
+total_minutes_in_year = days_in_year * hours_in_day * minutes_in_hour
+print(f"Total minutes in a year is {total_minutes_in_year}")
+
+name=input("Enter your name")
+print(f"Hi {name}! Welcome to the D43 batch")
+
+pounds = float(input("Please enter amount in pounds: "))
+dollars = pounds * 1.25
+print(f"£{pounds} are ${dollars}")
+
+
+
+
+
+
+
